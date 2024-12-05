3e0977e0f6bdd0c2d30f6b5c15405e1751d10784
Martin Tran <martintran2003@gmail.com>
2024-12-04 17:03:45 -0800

    remove hints components as they are placed into the Board component

--- nonogram-app/src/components/ColumnHints.jsx
+++
@@ -1,8 +0,0 @@
-function ColumnHints({ pieces, solved }) {
-  if (pieces.length == 0) return <></>;
-  const children = [];
-  children.push;
-  return <>{children}</>;
-}
-
-export default ColumnHints;
