2cd9b8a693eb7f4592dd50d3a96b3d9d1b0fe618
Martin Tran <martintran2003@gmail.com>
2025-01-12 15:53:06 -0800

    update to use env variables for server location

--- nonogram-app/src/App.jsx
+++ nonogram-app/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import GamePractice from "./components/GamePractice.jsx";
 import GameDaily from "./components/GameDaily.jsx";
